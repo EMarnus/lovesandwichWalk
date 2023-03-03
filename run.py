4476fa122e33c9ef52427c2cabccdfdec7fb77fb
Emile <bvcxzq1@gmail.com>
2023-03-03 21:31:34 +0000

    Add validate_data function for data entry get_sales_data.

--- run.py
+++ run.py
@@ -25,6 +25,25 @@ def get_sales_data():
     print("Exmple: 10,12,13,14,15,16\n")
 
     data_str = input("Enter your data here:")
-    print(f"The data entered is {data_str}")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values are requited, you privded {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data {e}, please try again.\n")
+
+
 
 get_sales_data()
